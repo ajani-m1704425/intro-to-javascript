3136bdeb5f1bd2b2efbec13c97cd6d8cdc7b7e01
Ajani Malik <malikolalekana4@gmail.com>
2022-06-24 08:10:14 +0100

    first commit

---
+++ intro.js
@@ -0,0 +1,9 @@
+var name = "AJANI MALIK OLALEKAN";
+var height = "5ft";
+var country = "Nigeria"
+
+
+
+console.log("Name:", name);
+console.log("Height:", height);
+console.log("Nationality:", country)
